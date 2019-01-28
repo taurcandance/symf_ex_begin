6a67488a31c7b4711c171e0f6866627b6c6a1ec4
php dev <andromitskevich@gmail.com>
2019-01-28 17:12:06 +0300

    1.0

--- tests/Util/ProductControllerTest.php
+++ tests/Util/ProductControllerTest.php
@@ -3,14 +3,14 @@
 namespace App\Tests;
 
 use App\Controller\ProductController\ProductController;
-use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductControllerTest extends TestCase
 {
     public function testBeDefinedProductClass()
     {
-        $product = new Product();
-        $this->assertInstanceOf(ProductController::class, $product);
+        $product = new ProductController();
+        $this->assertInstanceOf(AbstractController::class, $product);
     }
 }
